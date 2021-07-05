81f5047dc87145847645314be654ac569d5f3907
katskurka <kati.skurka@gmail.com>
2021-07-04 22:58:26 -0400

    wrote test for strings

--- test/strictEqual.test.js
+++ test/strictEqual.test.js
@@ -2,3 +2,12 @@ const describe = require('mocha').describe
 const it = require('mocha').it
 const expect = require('chai').expect
 const strictEqual = require('../strictEqual.js')
+
+describe('strictEqual', () => {
+  describe('result', () => {
+    it('when strings are strictly equal to one another, true is returned', () => {
+      let result = strictEqual ('boo', 'boo')
+      expect(result).to.equal(true)
+    })
+  })
+})
